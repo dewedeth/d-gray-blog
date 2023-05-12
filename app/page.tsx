b9a05a8cf7a33562c0a97c749fc086de3e7ddc58
dewedeth <yousefbacarro@icloud.com>
2023-05-12 16:49:25 -0400

    apos added

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold">Yousef</span>.
+          I&apos;m <span className="font-bold">Yousef</span>.
         </span>
       </p>
       {/* @ts-expect-error Server Component */}
